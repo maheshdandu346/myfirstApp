9104e3799d5bdcd0f7ac22beb51c2efb126bf8d5
maheshdandu346 <mahesh10346@gmail.com>
2021-05-16 23:36:26 +0530

    App

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
-var app= angular.module('myApp',[]);
-app.controller('myCtrl', function ($scope, $interval){
-    $scope.firstName = "Mahesh";
-    $scope.lastName = "Dandu";
-    $scope.time=new Date().toLocaleTimeString();
 
-    $interval(function () {
-      $scope.Time = new Date().toLocaleTimeString();
-  }, 1000);
-});
+(function () {
+'use strict';
+
+angular.module('', [])
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope', '$filter'];
+
+function LunchCheckController($scope, $filter) {
+  $scope.inputData = "";
+  $scope.myMessage = "";
+ 
+  $scope.getFood = function () {
+ //   var upCase = $filter('uppercase');
+  
+
+    $scope.inputData = $scope.inputData;
+  
+    
+    	if ($scope.inputData ==0) {
+    		$scope.myMessage = "Please enter data first!";
+    	} else {
+    		$scope.myMessage = myAnswer($scope.inputData)
+    	}
+    	
+  };
+
+function myAnswer (string){
+    var myFood = [];
+    var myReply = "";
+    var countIt = 0;
+    myFood = string.split(',');
+
+   for (var i = 0; i <myFood.length; i++) {
+      if (myFood[i].length > 0) {
+      	countIt++;
+      }
+
+    }
+	if (countIt <=3) {
+		myReply = "Enjoy!";
+	} else {
+		myReply = "Too much!";
+	} 
+	return myReply;
+
+};
+
+}
+
+})();
